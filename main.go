4c85807cc85091ffd5efee638d2526ccb738e163
Elena Grahovac <elena@grahovac.me>
2017-10-07 11:06:00 +0200

    Add env config

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/julienschmidt/httprouter"
 	"github.com/rumyantseva/devfest-2017/handlers"
@@ -11,11 +12,16 @@ import (
 // Run server: go build -o app && ./app
 // Try requests: curl http://127.0.0.1:8000/any
 func main() {
+	port := os.Getenv("PORT")
+	if len(port) == 0 {
+		log.Fatal("Port is not set.")
+	}
+
 	log.Print("Initialize service...")
 
 	router := httprouter.New()
 	router.GET("/", handlers.Home)
 
 	log.Print("We are ready to handle requests.")
-	http.ListenAndServe(":8000", router)
+	http.ListenAndServe(port, router)
 }
